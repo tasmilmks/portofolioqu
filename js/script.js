336dd0bd6b9a5db4e5d344badf06e1933af0167d
unknown <illungbriton@gmail.com>
2020-07-04 01:06:57 +0800

    menambahkan efek parallax

--- js/script.js
+++ js/script.js
@@ -28,3 +28,39 @@ $('a[href^="#"]').on('click', function(event) {
     }
 //menggunakan easing jquery
 });
+
+// parralax
+// about
+$(window).on('load',function(){
+    $('.pKiri').addClass('pmuncul');
+    $('.pKanan').addClass('pmuncul');
+});
+$(window).scroll(function(){
+    var wscroll=$(this).scrollTop();
+    // console.log(wscroll);
+    // jumbotron
+    $('.jumbotron img').css({
+        'transform':'translate(0px,'+wscroll/4+'%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform':'translate(0px,'+wscroll/2+'%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform':'translate(0px,'+wscroll/1.2+'%)'
+    });
+
+    // Portfolio
+    if(wscroll >$('.portfolio').offset().top-250){
+        // console.log('ok');
+       $('.portfolio .thumbnail').each(function(i){
+            setTimeout(function(){
+                $('.portfolio .thumbnail').eq(i).addClass('muncul');  
+            },300*(i+1));
+                 
+       });
+    }
+
+});
+// $('.portfolio .thumbnail').addClass('muncul');
